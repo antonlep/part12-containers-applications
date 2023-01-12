f88fa71bfe4ca57269822367d5b1ad2a08e7b825
antonlep <antonleppa@gmail.com>
2023-01-12 15:53:39 +0200

    ex 12.7

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -17,33 +17,41 @@ router.post('/', async (req, res) => {
   res.send(todo);
 });
 
-const singleRouter = express.Router();
+// const singleRouter = express.Router();
 
-const findByIdMiddleware = async (req, res, next) => {
-  const { id } = req.params
-  req.todo = await Todo.findById(id)
-  if (!req.todo) return res.sendStatus(404)
+// const findByIdMiddleware = async (req, res, next) => {
+//   const { id } = req.params
+//   req.todo = await Todo.findById(id)
+//   if (!req.todo) return res.sendStatus(404)
 
-  next()
-}
+//   next()
+// }
 
 /* DELETE todo. */
-singleRouter.delete('/', async (req, res) => {
+router.delete('/', async (req, res) => {
   await req.todo.delete()  
   res.sendStatus(200);
 });
 
 /* GET todo. */
-singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+router.get('/:id', async (req, res) => {
+  const { id } = req.params
+  const todo = await Todo.findById(id)
+  if (!todo) return res.sendStatus(404)
+  res.send(todo)
 });
 
 /* PUT todo. */
-singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+router.put('/:id', async (req, res) => {
+  const todo = req.body
+  const updatedTodo = await Todo.findByIdAndUpdate(req.params.id, todo, {
+    new: true,
+  })
+  if (!updatedTodo) return res.sendStatus(404)
+  res.send(updatedTodo)
 });
 
-router.use('/:id', findByIdMiddleware, singleRouter)
+//router.use('/:id', findByIdMiddleware, singleRouter)
 
 
 module.exports = router;
