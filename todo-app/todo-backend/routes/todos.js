faf4d871ac8b39c346923bfc93a6db0c93f2b035
antonlep <antonleppa@gmail.com>
2023-01-14 17:01:43 +0200

    ex 12.19

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -1,25 +1,25 @@
-const express = require('express');
-const { Todo } = require('../mongo')
-const router = express.Router();
-const redis = require('../redis')
+const express = require("express")
+const { Todo } = require("../mongo")
+const router = express.Router()
+const redis = require("../redis")
 
 /* GET todos listing. */
-router.get('/', async (_, res) => {
+router.get("/", async (_, res) => {
   const todos = await Todo.find({})
-  res.send(todos);
-});
+  res.send(todos)
+})
 
 /* POST todo to listing. */
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   const todo = await Todo.create({
     text: req.body.text,
-    done: false
+    done: false,
   })
-  const added_todos = await redis.getAsync('added_todos')
+  const added_todos = await redis.getAsync("added_todos")
   const new_todos = added_todos ? parseInt(added_todos) + 1 : 0
-  await redis.setAsync('added_todos', new_todos)
-  res.send(todo);
-});
+  await redis.setAsync("added_todos", new_todos)
+  res.send(todo)
+})
 
 // const singleRouter = express.Router();
 
@@ -32,30 +32,30 @@ router.post('/', async (req, res) => {
 // }
 
 /* DELETE todo. */
-router.delete('/', async (req, res) => {
-  await req.todo.delete()  
-  res.sendStatus(200);
-});
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params
+  await Todo.findByIdAndDelete(id)
+  res.sendStatus(200)
+})
 
 /* GET todo. */
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   const { id } = req.params
   const todo = await Todo.findById(id)
   if (!todo) return res.sendStatus(404)
   res.send(todo)
-});
+})
 
 /* PUT todo. */
-router.put('/:id', async (req, res) => {
+router.put("/:id", async (req, res) => {
   const todo = req.body
   const updatedTodo = await Todo.findByIdAndUpdate(req.params.id, todo, {
     new: true,
   })
   if (!updatedTodo) return res.sendStatus(404)
   res.send(updatedTodo)
-});
+})
 
 //router.use('/:id', findByIdMiddleware, singleRouter)
 
-
-module.exports = router;
+module.exports = router
