b83e1665a9e77a265273c18373eeabbfe65302bd
antonlep <antonleppa@gmail.com>
2023-01-14 13:59:41 +0200

    ex 12.16

--- todo-app/todo-backend/routes/index.js
+++ todo-app/todo-backend/routes/index.js
@@ -1,19 +1,19 @@
-const express = require('express');
-const router = express.Router();
+const express = require("express")
+const router = express.Router()
 
-const configs = require('../util/config')
-const redis = require('../redis')
+const configs = require("../util/config")
+const redis = require("../redis")
 
 let visits = 0
 
 /* GET index data. */
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   visits++
 
   res.send({
     ...configs,
-    visits
-  });
-});
+    visits,
+  })
+})
 
-module.exports = router;
+module.exports = router
